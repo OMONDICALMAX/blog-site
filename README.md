2bc8ae658073c9c0c272aad9867cd9fe0ef10a2e
Calmax Omondi <calmax.omondi@student.moringaschool.com>
2026-06-05 07:44:58 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+└── App
+    ├── Header
+    ├── About
+    └── Article List
+        └── Article
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <div className="bar">
+            <ul>
+                <li><button className="headerBTN">Home</button></li>
+                <li><button className="headerBTN">Blog</button></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
